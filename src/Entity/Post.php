6100aa6b81e7e71b0a161f77cb24f3a79fd459d6
Rodrigo Gutierrez <rodrigo.gutierrez@up.ac.pa>
2021-03-03 13:57:04 -0500

    Function crud for posts

---
+++ src/Entity/Post.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PostRepository::class)
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $detail;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $publisheddate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $owner;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDetail(): ?string
+    {
+        return $this->detail;
+    }
+
+    public function setDetail(string $detail): self
+    {
+        $this->detail = $detail;
+
+        return $this;
+    }
+
+    public function getPublisheddate(): ?\DateTimeInterface
+    {
+        return $this->publisheddate;
+    }
+
+    public function setPublisheddate(\DateTimeInterface $publisheddate): self
+    {
+        $this->publisheddate = $publisheddate;
+
+        return $this;
+    }
+
+    public function getOwner(): ?string
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(string $owner): self
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+}
